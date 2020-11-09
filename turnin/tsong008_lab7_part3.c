336680338fb64afb8604eb7467c578f6dfd7f96a
Tinghui Song <tinghuigz10@gmail.com>
2020-11-08 22:53:57 -0800

    Added demo links

--- turnin/tsong008_lab7_part3.c
+++ turnin/tsong008_lab7_part3.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo URL: 
+ *	Demo URL: https://drive.google.com/file/d/18DKZmszKyzGdX5qrrQEztJ_MU9WqEqSc/view?usp=sharing
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
--- turnin/tsong008_lab7_part4.c
+++ turnin/tsong008_lab7_part4.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo URL: 
+ *	Demo URL: https://drive.google.com/file/d/1pGitnKDq4t3C1buvfdO76tUVu9hY2GfD/view?usp=sharing
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
